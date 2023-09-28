df66932e02240489569266d98ebd45d59d60d62f
João Pedro <125619229+JoaoPedroCanavez@users.noreply.github.com>
2023-09-27 21:05:49 -0300

    first commit new repository

---
+++ webProjectSite01/assets/js/script.js
@@ -0,0 +1,71 @@
+/*var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
+
+console.log("Script");
+*/
+
+//console.log("Script")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nome = document.querySelector("#nome")
+
+
+
+const campoQtd = document.querySelector("#qtde");
+const output = document.querySelector("output")
+const campoPrazo = document.querySelector("#prazo")
+const campoJs = document.querySelector("#js")
+campoJs.addEventListener("change", calcular)
+
+
+campoPrazo.addEventListener("input",   function() {
+    
+  const labelPrazo = document.querySelector("label[for = 'prazo']")
+
+  labelPrazo.innerHTML = `Quanto gostou :  ${campoPrazo.value} %`
+  calcular()
+})
+
+
+campoQtd.addEventListener("change" , calcular )
+
+function calcular(){
+
+console.log("Calcular Estrelas")
+let qtde = campoQtd.value
+let valor = qtde * 1;
+
+if(campoJs.checked){
+
+  alert("Voce recebera o alerta no email")
+  
+  }
+
+output.innerHTML=  `${valor} Estrelas`
+}
+
